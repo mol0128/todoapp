d74be70799f5f53be7cc4f512f937d9a7acebf85
室井博也 <>
2020-09-10 15:30:08 +0900

    boardとユーザーの紐付け、homeをboardsに変更

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,2 +1,6 @@
 class Board < ApplicationRecord
+
+
+  belongs_to :user
+  
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Board.create({title: 'テスト', content: 'testtesttest'})
+Board.create({title: 'テスト2', content: 'testtesttest2222'})
