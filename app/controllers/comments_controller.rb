d27c64ec1baa5c1fdea737ec470881c0e5f07ab0
室井博也 <>
2020-09-15 17:32:56 +0900

    コメント投稿

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,4 +3,19 @@ class CommentsController < ApplicationController
     task = Task.find(params[:task_id])
     @comment = task.comments.build
   end
+
+  def create
+    @task = Task.find(params[:task_id])
+    @comment = @task.comments.build(comment_params)
+    if @comment.save
+      redirect_to board_task_path(board_id: @task.board_id, id: @task.id), notice: 'コメントを追加'
+    else
+      render :new
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:content).merge(user_id: current_user.id)
+  end
 end
