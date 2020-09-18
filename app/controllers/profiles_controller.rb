d401813d3a7ee371f278610493b0ef2eaa5707a2
室井博也 <>
2020-09-18 23:12:12 +0900

    プロフィールの作成・更新

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -8,4 +8,25 @@ before_action :authenticate_user!
   def edit
     @profile = current_user.build_profile
   end
+
+  def update
+    @profile = current_user.build_profile(profile_params)
+    if @profile.save
+      redirect_to profile_path, notice: 'プロフィールを編集しました'
+    else
+      flash.now[:alert] = '更新できませんでした'
+      render :edit
+    end
+  end
+
+  private
+
+  def profile_params
+    params.require(:profile).permit(
+      :name,
+      :belongs,
+      :gender,
+      :subscribed
+    )
+  end
 end
